99372bd641120b0a9a43491f344d04da0c5928e0
HOG-StarCraft <W3389823633@hotmail.com>
2024-10-13 18:35:21 +0800

    add HTML tools&games

--- .vitepress/nav.mjs
+++ .vitepress/nav.mjs
@@ -11,4 +11,22 @@ export default [
         { text: 'Novel', link: 'https://hog-starwatch.github.io/Novel/', target: '_self', },
       ],
     },
+    { text: '小工具',
+      items: [
+        { text: '零宽隐写', link: '零宽隐写.html', target: '_self', },
+        { text: '加密解密', link: '加密解密.html', target: '_self', },
+        { text: 'Zip',
+          items: [
+            { text: '文件多合一', link: 'Zip.html', target: '_self', },
+            { text: '文件夹合一', link: 'Zip-dir.html', target: '_self', },
+          ],
+        },
+      ],
+    },
+    { text: '小游戏',
+      items: [
+        { text: 'Wordle', link: 'Wordle.html', target: '_self', },
+        { text: '猜数字', link: '猜数字.html', target: '_self', },
+      ],
+    },
 ];
